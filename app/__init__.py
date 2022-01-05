75bf2d51c3dc1416139b730202aa772ba8ed0639
LiuZhen <sakura.sv0123@gmail.com>
2022-01-05 20:10:57 +0800

    add app init func

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    cfg = config[config_name]
+    app.config.from_object(cfg)
+    cfg.init_app(app)
+    logging_conf(app)
+    db.init_app(app)
+
+
+def logging_conf(app):
+    formatter = logging.Formatter(app.config['LOGGING_FORMATTER'])
+    debug_log = os.path.join(app.config['BASE_DIR'],
+                             app.config['LOGGING_ERROR_FILE'])
+    debug_handler = RotatingFileHandler(debug_log)
+    debug_handler.setLevel(logging.DEBUG)
+    debug_handler.setFormatter(formatter)
+    app.logger.addHandler(debug_handler)
+
+    error_log = os.path.join(app.config['BASE_DIR'],
+                             app.config['LOGGING_ERROR_FILE'])
+    error_handler = RotatingFileHandler(error_log)
+    error_handler.setLevel(logging.ERROR)
+    error_handler.setFormatter(formatter)
+    app.logger.addHandler(error_handler)
+
+    if app.config['DEBUG']:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(formatter)
+        app.logger.addHandler(console_handler)
---
+++ config.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+"""Config file."""
+
+import os
+
+
+class Config:
+    """Basic config."""
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    LOGGING_FORMATTER = '%(asctime)s - %(name)s - %(levelname)s - %(message)s' \
+                        ' [in %(pathname)s:%(lineno)d]'
+    LOGGING_ERROR_FILE = 'data/app_error.log'
+    LOGGING_DEBUG_FILE = 'data/app_debug.log'
+
+    @staticmethod
+    def init_app(app):
+        """Nothing."""
+        pass
+
+
+class DevelopmentConfig(Config):
+    """Dev config."""
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PYMYSQL_DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class ProductionConfig(Config):
+    """Production config."""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PYMYSQL_DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+
+    'default': ProductionConfig
+}
